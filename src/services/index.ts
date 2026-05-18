42ee04eb23ac3090f4609884c163f86a602611d6
ding <ding@ding.com>
2026-05-18 16:23:12 +0800

    add 第1次

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,6 @@
-export { MedicineService } from './MedicineService';
-export { MedicalRecordService } from './MedicalRecordService';
-export { BillService } from './BillService';
+export { MedicineService } from "./MedicineService";
+export { MedicalRecordService } from "./MedicalRecordService";
+export { BillService } from "./BillService";
+export { StatisticsService } from "./StatisticsService"rvice';
+export { BillPaymentService } from './bill-payment.service';
 export { StatisticsService } from './StatisticsService';
